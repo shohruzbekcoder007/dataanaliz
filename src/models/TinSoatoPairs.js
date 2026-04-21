ac35c586307084d6b7cf6f8668377335a7524422
shohruzbekcoder007 <shohruzbekcoder007@gmail.com>
2026-04-21 20:54:19 +0500

    yana bitta model qushildi

---
+++ src/models/TinSoatoPairs.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const tinSoatoPairsSchema = new mongoose.Schema(
+  {
+    tin: { type: String, index: true },
+    soato: { type: String, index: true },
+    total_land_area: { type: Number, default: 0 },
+    gis_area_ha_total: { type: Number, default: 0 },
+    arable_area_size_total: { type: Number, default: 0 },
+  },
+  {
+    collection: "tin_soato_pairs",
+    timestamps: false,
+  }
+);
+
+tinSoatoPairsSchema.index({ tin: 1, soato: 1 });
+
+module.exports = mongoose.model("tin_soato_pairs", tinSoatoPairsSchema);
